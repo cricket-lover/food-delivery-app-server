1115cb27e4b06235766c1eae165b36cc6db4711d
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-08-25 15:03:21 +0530

    add morgan for logging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const morgan = require("morgan");
 const { authenticateToken } = require("./middleware");
 const {
   getAllRestaurants,
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
