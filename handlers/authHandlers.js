5dddd85323826fa6820e8c4b9505f5cefa6843ed
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-09-12 19:34:34 +0530

    add path for fs

--- handlers/authHandlers.js
+++ handlers/authHandlers.js
@@ -20,11 +20,11 @@ const signupHandler = async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     users.push({ username, password: hashedPassword });
-    // fs.writeFileSync(
-    //   path.resolve(__dirname, "../data/users.json"),
-    //   JSON.stringify(users, null, 2),
-    //   "utf8"
-    // );
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/users.json"),
+      JSON.stringify(users, null, 2),
+      "utf8"
+    );
     return res.status(201).json({ username, email });
   } catch (error) {
     return res.status(500).json(error);
